8b849bfa8ff8bab10ab6e42b5a9f1f8cb115d89a
Francesco Biscazzo <11650573+franbis@users.noreply.github.com>
2025-06-21 04:50:18 +0200

    Modified /help

--- bot.py
+++ bot.py
@@ -114,6 +114,8 @@ def send_help(msg):
 		'/tts		-	Make a voice message out of a textual one\n'
 		'/img		-	Generate an image\n'
 
+		"\n**NOTE**: You don't need to use /chat or /achat, simply reply with text or voice instead."
+
 		'\n\nYou can find the full list of commands '
 		'[here](https://github.com/franbis/TODO/commands.md)'
 		'.'
